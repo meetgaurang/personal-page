bfb4a22b580076c3a695c3b2058913b0ee58a44a
Gaurang Patel <gaurang.patel@reckon.com>
2024-09-01 20:18:57 +1000

    Updated pic and style touch-ups

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        golden: {
+          50: "#d3b261",
+        },
+        glowingblack: {
+          50: "#2C3333",
+        },
+      },
+    },
   },
   plugins: [],
 };
